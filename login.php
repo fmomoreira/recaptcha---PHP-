f56460f35826fc4cb0aca1768f1644923caf23b0
fmomoreira <felipemoreira.frontend@gmail.com>
2021-02-12 11:40:16 -0300

    upload arquivos para configuração do recaptcha

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+
+$email = $_POST['inputEmail'];
+
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+    "secret"=>"SUA PRIVATEKEY AQUI",
+    "response"=>$_POST["g-recaptcha-response"],
+    "remoteip"=>$_SERVER["REMOTE_ADDR"]
+)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$recaptcha = json_decode(curl_exec($ch), true);
+
+curl_close($ch);
+
+if($recaptcha["success"] === true){
+    //aqui voce pode salvar no banco
+    echo "Ok: ".$_POST["inputEmail"];
+}else{
+   header("Location: teste.php");
+}
+
+
+
+?>
---
+++ teste.php
@@ -0,0 +1,11 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+<form action="./login.php" method="post" >
+<input type="email" name="inputEmail">
+<button type="submit">Enviar</button>
+<div class="g-recaptcha" data-sitekey="SUA PUBLICKEYAQUI"></div>
+
+
+</form>
+
